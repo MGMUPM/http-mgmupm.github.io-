7c6bf39483fded7269d5071411a5dcd29ee2cc98
Manuel Garcia Montes <garcia.montes.manuel@gmail.com>
2016-10-04 20:08:49 +0200

    Pequeños cambios

--- common.js
+++ common.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
   document.getElementById("Inicio").addEventListener("click", function(){
     document.getElementById("map").style = "display: none";
     document.getElementById("Start").style = "display: block";
+    document.getElementById("Teledeteccion").style = "display: none";
   })
   document.getElementById("Mapa").addEventListener("click", function(){
     document.getElementById("Start").style = "display: none";
     document.getElementById("map").style = "display: block";
+    document.getElementById("Teledeteccion").style = "display: none";
+  })
+  document.getElementById("TDT").addEventListener("click", function(){
+    document.getElementById("Start").style = "display: none";
+    document.getElementById("Teledeteccion").style = "display: block";
+    document.getElementById("map").style = "display: none";
   })
 })
